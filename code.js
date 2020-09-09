d830efe5303d769a7d5faee1ad557d3e3f195db8
Arisa <arisa.pote.831@gmail.com>
2020-09-09 14:49:08 +0900

    Generate 'test x ' text by pushing the button

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+// it will then generate that many texts on the screen.
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
@@ -11,16 +11,21 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+    // if (msg.type === 'generate-texts') {
+    const nodes = [];
+    for (let i = 0; i < msg.count; i++) {
+        const text = figma.createText();
+        figma.loadFontAsync({
+            family: "Roboto", style: "Regular"
+        }).then(() => {
+            text.x = i * 50;
+            return text.characters = `test x ${i}`;
+        });
+        // figma.currentPage.selection = nodes;
+        // figma.viewport.scrollAndZoomIntoView(nodes);
+        figma.currentPage.appendChild(text);
+        nodes.push(text);
+        console.log(`${i}times`);
     }
+    // }
 };
