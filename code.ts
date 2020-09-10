70d1530f43b1953230b47387676a9a314a28a6d1
Arisa <arisa.pote.831@gmail.com>
2020-09-10 10:19:17 +0900

    Generate texts only when user selects text layer

--- code.ts
+++ code.ts
@@ -16,61 +16,71 @@ figma.ui.onmessage = msg => {
   // your HTML page is to use an object with a "type" property like this.
 
 
-  const nodes: SceneNode[] = [];
+  // const nodes: SceneNode[] = [];
 
-  if (msg.type === 'word-selected') {
+  // 選択レイヤーが単一のテキストの時のみ発動する
+  if (figma.currentPage.selection[0] && figma.currentPage.selection[0].type === "TEXT") {
 
-    for (let i = 0; i < msg.count; i++) {
-      const text: TextNode = figma.createText();
+    if (msg.type === 'word-selected') {
 
-      figma.loadFontAsync({
-        family: "Roboto", style: "Regular"
-      }).then(() => {
-        text.x = i * 50;
-        return text.characters = `test x ${i}`;
-      })
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-      figma.currentPage.appendChild(text);
-      nodes.push(text);
-      console.log(`${i}times`);
+      for (let i = 0; i < msg.count; i++) {
+        const nodes: SceneNode[] = [];
+        const text: TextNode = figma.createText();
+
+        figma.loadFontAsync({
+          family: "Roboto",
+          style: "Regular"
+        }).then(() => {
+          text.x = i * 50;
+          text.characters = `Test x ${i}`;
+          nodes.push(text);
+          figma.currentPage.appendChild(text);
+        })
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+        console.log(`${i}times`);
+      }
+      console.log(`Word is selected = ${msg.word}`);
+    } else if (msg.type === 'sentence-selected') {
+
+      for (let i = 0; i < msg.count; i++) {
+        const nodes: SceneNode[] = [];
+        const text: TextNode = figma.createText();
+
+        figma.loadFontAsync({
+          family: "Roboto",
+          style: "Regular"
+        }).then(() => {
+          text.x = i * 50;
+          text.characters = `This is test x ${i}`;
+          nodes.push(text);
+          figma.currentPage.appendChild(text);
+        })
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+        console.log(`${i}times`);
+      }
+      console.log(`Sentence is selected = ${msg.sentence}`);
+    } else if (msg.type === 'para-selected') {
+
+      for (let i = 0; i < msg.count; i++) {
+        const nodes: SceneNode[] = [];
+        const text: TextNode = figma.createText();
+
+        figma.loadFontAsync({
+          family: "Roboto",
+          style: "Regular"
+        }).then(() => {
+          text.x = i * 50;
+          text.characters = `This is test. And this is paragraph. x ${i}`;
+          nodes.push(text);
+          figma.currentPage.appendChild(text);
+        })
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+        console.log(`${i}times`);
+      }
+      console.log(`Paragraph is selected = ${msg.para}`);
     }
-    console.log(`Word is selected = ${msg.word}`);
-  } else if (msg.type === 'sentence-selected') {
-
-    for (let i = 0; i < msg.count; i++) {
-      const text: TextNode = figma.createText();
-
-      figma.loadFontAsync({
-        family: "Roboto", style: "Regular"
-      }).then(() => {
-        text.x = i * 50;
-        return text.characters = `test x ${i}`;
-      })
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-      figma.currentPage.appendChild(text);
-      nodes.push(text);
-      console.log(`${i}times`);
-    }
-    console.log(`Sentence is selected = ${msg.sentence}`);
-  } else if (msg.type === 'para-selected') {
-
-    for (let i = 0; i < msg.count; i++) {
-      const text: TextNode = figma.createText();
-
-      figma.loadFontAsync({
-        family: "Roboto", style: "Regular"
-      }).then(() => {
-        text.x = i * 50;
-        return text.characters = `test x ${i}`;
-      })
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-      figma.currentPage.appendChild(text);
-      nodes.push(text);
-      console.log(`${i}times`);
-    }
-    console.log(`Paragraph is selected = ${msg.para}`);
   }
 };
