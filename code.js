4fc09465f0e44108ec6ad9c7236b516c1f476a9d
Kite <ixkaito@gmail.com>
2020-09-16 18:19:07 +0900

    Remove the ui size

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 300, height: 400 });
+figma.showUI(__html__);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
--- code.ts
+++ code.ts
@@ -8,7 +8,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 300, height: 400 });
+figma.showUI(__html__);
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
