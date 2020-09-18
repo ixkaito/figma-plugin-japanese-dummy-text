094b31a6a82ece0ae2ce1d1fc40ac7aeac1b42b9
Kite <ixkaito@gmail.com>
2020-09-18 13:30:13 +0900

    Update ui

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,13 +1,25 @@
 // import { css } from '@emotion/core'
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+// import * as React from 'react'
+// import * as ReactDOM from 'react-dom'
 
-const App = () => {
-  return (
-    <div>
-      <div>test</div>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//       <div
+//         css={css`
+//           display: flex;
+//         `}
+//       >
+//         <input type="number" id="count" value="1"></input>
+//         <select id="text-unit">
+//           <option value="character">字</option>
+//           <option value="sentence">文</option>
+//           <option value="paragraph">段落</option>
+//         </select>
+//         lorem
+//       </div>
+//     </div>
+//   )
+// }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// ReactDOM.render(<App />, document.getElementById('app'))
