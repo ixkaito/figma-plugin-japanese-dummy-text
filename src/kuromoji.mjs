9a02d1770f26617c09a72bfe800cccf3e321a9d2
Kite <ixkaito@gmail.com>
2020-09-20 18:26:43 +0900

    Update kuromoji.mjs

--- src/kuromoji.mjs
+++ src/kuromoji.mjs
@@ -1,12 +1,15 @@
-import kuromoji from 'kuromoji'
+import path from 'path'
 import fs from 'fs'
+import kuromoji from 'kuromoji'
 
-const input = fs.readFileSync('input.txt', 'utf8').replace(/(\r\n|\n|\r)/gm, '')
+const input = fs
+  .readFileSync('input.txt', 'utf8')
+  .replace(/(\r\n|\n|\r)/gm, '')
 
 kuromoji
   .builder({ dicPath: '../node_modules/kuromoji/dict/' })
   .build((err, tokenizer) => {
-    const output = JSON.stringify(tokenizer.tokenize(input), null, 2)
+    const output = JSON.stringify(tokenizer.tokenize(input))
     fs.writeFile('text.json', output, (err) => {
       if (err) {
         console.error(err)
