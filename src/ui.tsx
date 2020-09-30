effbd7568f1a1b30bc44d8c1457b1fddb3cb7014
Kite <ixkaito@gmail.com>
2020-09-30 10:14:48 +0900

    Improve npm-script for React

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,25 +1,29 @@
-// import { css } from '@emotion/core'
-// import * as React from 'react'
-// import * as ReactDOM from 'react-dom'
+import { css } from '@emotion/core'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-// const App = () => {
-//   return (
-//     <div>
-//       <div
-//         css={css`
-//           display: flex;
-//         `}
-//       >
-//         <input type="number" id="count" value="1"></input>
-//         <select id="text-unit">
-//           <option value="character">字</option>
-//           <option value="sentence">文</option>
-//           <option value="paragraph">段落</option>
-//         </select>
-//         lorem
-//       </div>
-//     </div>
-//   )
-// }
+const App = () => {
+  return (
+    <div>
+      <div id="error" css={css`
+        color: #f24822;
+        padding: 14px;
+        text-align: center;
+      `}>
+        <p css={css`
+          font-size: 16px;
+        `}>
+          Please select some text or rectangle layers.
+        </p>
+        <p css={css`
+          margin-top: 8px;
+        `}>
+          テキストもしくは矩形レイヤーを<br />
+          1つ以上選択してください。
+        </p>
+      </div>
+    </div>
+  )
+}
 
-// ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin')
 
 module.exports = (env, argv) => ({
+  mode: argv.mode === 'production' ? 'production' : 'development',
   // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
   entry: {
@@ -27,7 +28,7 @@ module.exports = (env, argv) => ({
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
